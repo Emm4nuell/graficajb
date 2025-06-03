8120d00f68f7dfc86bb3de8ded46bdbaedf1dd4f
edilsonjrcode <2022111510116@iesp.edu.br>
2025-06-03 01:30:33 -0300

    feat(components): cria componente de botão personalizado

---
+++ src/components/ButtomBlue/ButtomBlue.tsx
@@ -0,0 +1,7 @@
+import './ButtomBlue.css'
+
+export default function ButtomBlue({text_button}){
+    return (
+        <button type='submit' style={{margin: "0 auto"}}>{text_button}</button>
+    )
+}
