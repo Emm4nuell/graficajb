669882040f2702d4dad38db19ddce7aac680ff76
edilsonjrcode <2022111510116@iesp.edu.br>
2025-06-04 11:59:48 -0300

    style(ButtomBlue): migra estilos CSS para Tailwind

--- src/components/ButtomBlue/ButtomBlue.tsx
+++ src/components/ButtomBlue/ButtomBlue.tsx
@@ -2,6 +2,6 @@ import './ButtomBlue.css'
 
 export default function ButtomBlue({text_button}){
     return (
-        <button type='submit' style={{margin: "0 auto"}}>{text_button}</button>
+        <button type='submit' className="w-[14rem] h-[3.9375rem] rounded-[4.78125rem] bg-[#00A8EA] text-white text-base font-semibold leading-[1.625rem] cursor-pointer" style={{margin: "0 auto"}}>{text_button}</button>
     )
 }
