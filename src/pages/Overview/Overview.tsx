f3429a1b2e30d42150525e70622d8d7d53a1339a
edilsonjrcode <2022111510116@iesp.edu.br>
2025-06-04 11:55:24 -0300

    feat(overview): cria página de overview

---
+++ src/pages/Overview/Overview.tsx
@@ -0,0 +1,11 @@
+import './Overview.css'
+import CustomInputText from "../../components/CustomInputText/CustomInputText";
+import ButtomBlue from '../../components/ButtomBlue/ButtomBlue';
+import Header from '../../components/Header/Header';
+export default function OverviewPage() {
+  return (
+    <>
+      <Header/>
+    </>
+  );
+}
