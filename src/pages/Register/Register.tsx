e417bdb769da5b34e2347aaf5c18587faa6e2221
edilsonjrcode <2022111510116@iesp.edu.br>
2025-06-04 11:54:24 -0300

    style(register): migra estilos CSS para Tailwind

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -11,15 +11,15 @@ export default function RegisterPage() {
           <p>Já posssui uma conta? <a href='#'>Faça login</a></p>
         </div>
 
-        <div style={{display: "flex", flexDirection: "column", gap: "1rem", width: "36rem"}}>
-          <div style={{display: "flex", flexDirection: "row", gap: "0.94rem"}}>
+        <div className="flex flex-col gap-4 w-[36rem]">
+          <div className="flex flex-row gap-[0.94rem]">
             <CustomInputText type_input={"text"} label={"Nome"} placeholder={"Insira seu nome"}></CustomInputText>
             <CustomInputText type_input={"text"} label={"Sobrenome"} placeholder={"Insira seu sobrenome"}></CustomInputText>
           </div>
-          <div style={{display: "flex", flexDirection: "row"}}>
+          <div className="flex flex-row">
             <CustomInputText type_input={"email"} label={"Email"} placeholder={"Insira seu email"}></CustomInputText>
           </div>
-          <div style={{display: "flex", flexDirection: "row"}}>
+          <div className="flex flex-row">
             <CustomInputText type_input={"password"} label={"Senha"} placeholder={"Insira sua senha"}></CustomInputText>
           </div>
           <ButtomBlue text_button="Criar Conta"></ButtomBlue>
