b70440a5aa04b9601e1980c861122b645ea06fd3
edilsonjrcode <2022111510116@iesp.edu.br>
2025-06-17 23:32:49 -0300

    style(ButtomBlue): insere classe para aplicar estilo próprio

--- src/components/ButtomBlue/ButtomBlue.tsx
+++ src/components/ButtomBlue/ButtomBlue.tsx
@@ -1,7 +1,13 @@
 import './ButtomBlue.css'
 
-export default function ButtomBlue({text_button}){
+type ButtonBlueProps = {
+  text_button: string;
+  onClick?: () => void;
+  disabled?: boolean;
+};
+
+export default function ButtomBlue({text_button, onClick, disabled}: ButtonBlueProps){
     return (
-        <button type='submit' style={{margin: "0 auto"}}>{text_button}</button>
+        <button className='buttomBlue' type='submit' disabled={disabled} onClick={onClick} style={{margin: "0 auto", cursor: disabled ? "not-allowed" : "pointer"}}>{text_button}</button>
     )
 }
