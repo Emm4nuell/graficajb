974f416fff1eb222a87f5e371744b2305fbf9544
edilsonjrcode <2022111510116@iesp.edu.br>
2025-06-19 00:13:11 -0300

    feat(main) - adiciona estilos do prime-react

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,14 @@ import RegisterPage from "./pages/Register/Register.tsx";
 import SignInPage from "./pages/SignIn/SignIn.tsx";
 import OverviewPage from "./pages/Overview/Overview.tsx";
 import OpportunityPage from "./pages/Opportunity/Opportunity.tsx";
+import MyResumePage from "./pages/MyResume/MyResume.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute.tsx";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; 
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 const router = createBrowserRouter([
   { path: "/", element: <RegisterPage /> },
@@ -24,6 +28,7 @@ const router = createBrowserRouter([
   {
     element: <PrivateRoute />, // <- tudo abaixo aqui é privado
     children: [
+        { path: "myresume", element: <MyResumePage /> },
     ],
   },
 ]);
